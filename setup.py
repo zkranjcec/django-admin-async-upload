63fa846688fffdd2430bc46eeadc1c0bff0980ff
Alexey Strelkov <cpr.al.exe@gmail.com>
2020-09-07 17:23:23 +0100

    patched "chunk uploaded" error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-admin-async-upload',
-    version='3.0.2',
+    version='3.0.3',
     packages=['admin_async_upload'],
     include_package_data=True,
     package_data={
